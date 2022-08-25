0bead582b1b43c2254f028c2f52e753b7ce370b1
Joltra <MinnyMouseXDmail.com>
2022-08-25 12:32:30 +0200

    Added Actor class

---
+++ actor/Actor.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod, ABC
+
+
+class Actor(ABC):
+    @abstractmethod
+    def move_up(self):
+        pass
+
+    @abstractmethod
+    def move_down(self):
+        pass
+
+    @abstractmethod
+    def move_left(self):
+        pass
+
+    @abstractmethod
+    def move_right(self):
+        pass
+
+
+    @abstractmethod
+    def eat(self):
+        pass
