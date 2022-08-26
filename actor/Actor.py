92876a50fa309b79f7e33324c03967a86cc57680
Joltra <MinnyMouseXDmail.com>
2022-08-26 10:49:31 +0200

    Added return types

--- actor/Actor.py
+++ actor/Actor.py
@@ -3,22 +3,21 @@ from abc import abstractmethod, ABC
 
 class Actor(ABC):
     @abstractmethod
-    def move_up(self):
+    def move_up(self) -> None:
         pass
 
     @abstractmethod
-    def move_down(self):
+    def move_down(self) -> None:
         pass
 
     @abstractmethod
-    def move_left(self):
+    def move_left(self) -> None:
         pass
 
     @abstractmethod
-    def move_right(self):
+    def move_right(self) -> None:
         pass
 
-
     @abstractmethod
-    def grow(self):
+    def grow(self) -> None:
         pass
