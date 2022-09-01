49b67944ec98f83d49d9211a1c08f6e1c2dd45c4
Joltra <MinnyMouseXDmail.com>
2022-09-01 21:52:59 +0200

    Add comments

--- src/actor/Actor.py
+++ src/actor/Actor.py
@@ -4,20 +4,35 @@ from abc import abstractmethod, ABC
 class Actor(ABC):
     @abstractmethod
     def move_up(self) -> None:
+        """
+        Moves the actor up.
+        """
         pass
 
     @abstractmethod
     def move_down(self) -> None:
+        """
+        Moves the actor down.
+        """
         pass
 
     @abstractmethod
     def move_left(self) -> None:
+        """
+        Moves the actor left.
+        """
         pass
 
     @abstractmethod
     def move_right(self) -> None:
+        """
+        Moves the actor right.
+        """
         pass
 
     @abstractmethod
     def grow(self) -> None:
+        """
+        Actor grows.
+        """
         pass
