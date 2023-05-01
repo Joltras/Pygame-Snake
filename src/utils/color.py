303210db40cc0318308aa0a3e5dacb97c9e85bb1
Joltras <MinnyMouseXD@gmail.com>
2023-05-01 11:13:42 +0200

    Create file for color enum

---
+++ src/utils/color.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class Color(Enum):
+    ORANGE = (255, 140, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    DARK_GREEN = (0, 100, 0)
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    GRAY = (150, 150, 150)
+    LIGHT_GRAY = (200, 200, 200)
+    DARK_GRAY = (100, 100, 100)
+    YELLOW = (250, 250, 55)
+    VIOLET = (148, 0, 211)
