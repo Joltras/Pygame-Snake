493879021837285e242a2cd4c6fb632b709f43dd
Joltras <MinnyMouseXD@gmail.com>
2023-05-01 11:17:14 +0200

    Fix imports

--- src/game.py
+++ src/game.py
@@ -6,12 +6,13 @@ from pygame.surface import Surface, SurfaceType
 
 from food import Food
 from game_field import GameField
-from src.utils.globals import Color, GameState, SQUARE_SIZE, BUTTON_WIDTH, BUTTON_HEIGHT, FIELD_COLOR
+from src.utils.color import Color
+from src.utils.game_state import GameState
+from src.utils.globals import SQUARE_SIZE, BUTTON_WIDTH, BUTTON_HEIGHT, FIELD_COLOR
 from src.actor.snake_actor import Snake
 from src.commands.command import Command
 from src.commands.grow import GrowCommand
 from src.commands.move_commands import MoveUpCommand, MoveDownCommand, MoveLeftCommand, MoveRightCommand
-from src.ui import message
 from src.ui.text_button import TextButton, ImageButton
 from src.ui.message import MessageDisplayer
 
