7b42758d002c6090bbae6c2bbce8e62bfbdfe58b
Joltras <MinnyMouseXD@gmail.com>
2023-05-06 11:49:00 +0200

    Use smaller height for the game

--- src/main.py
+++ src/main.py
@@ -5,5 +5,5 @@ from game import Game
 if __name__ == "__main__":
     pygame.init()
     info = pygame.display.Info()
-    game = Game(info.current_w, info.current_h)
+    game = Game(info.current_w, info.current_h - 300)
     game.run()
